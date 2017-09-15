f310e465235e678de4008f9935f8ca1132732ea0
jpazdera <jpazdera@haverford.edu>
2017-09-15 16:13:26 -0400

    Added script to detect recently modified sessions

---
+++ identify_modified_participants.py
@@ -0,0 +1,51 @@
+import os
+import json
+import datetime as dt
+from time import time
+from glob import glob
+
+
+def identify_modified_participants_ltpFR2(day_limit=7):
+
+    # Define parameters of experiment
+    exp_dir = '/data/eeg/scalp/ltp/ltpFR2/'
+    naming_scheme = 'LTP[0-9][0-9][0-9]'
+    n_sess = 24
+    sessions = range(n_sess)
+
+    # Find all ltpFR2 subject directories
+    subj_dirs = glob(os.path.join(exp_dir, naming_scheme))
+
+    # Get current timestamp
+    current_time = dt.datetime.fromtimestamp(time())
+
+    # Determine which sessions from each subject have been modified
+    modified = dict()
+    for path in subj_dirs:
+        subj = os.path.basename(path)
+        for i in sessions:
+            # Locate files to check for modifications
+            sess_dir = os.path.join(path, 'session_%d' % i)
+            files_of_interest = glob(os.path.join(sess_dir, '*.ann')) + \
+                                glob(os.path.join(sess_dir, 'session.log')) + \
+                                glob(os.path.join(sess_dir, 'events.mat')) + \
+                                glob(os.path.join(sess_dir, 'eeg', '*.bz2'))
+
+            # Check each file for recent modifications
+            for f in files_of_interest:
+                last_modified = dt.datetime.fromtimestamp(os.path.getmtime(f))
+                days_since_modification = (current_time - last_modified).days
+                # Record modified subjects and sessions in a dictionary
+                if days_since_modification < day_limit:
+                    if subj not in modified:
+                        modified[subj] = [i]
+                    else:
+                        modified[subj].append(i)
+                    break
+
+    with open(os.path.join(exp_dir, 'recently_modified.json'), 'w') as f:
+        json.dump(modified, f)
+
+
+if __name__ == "__main__":
+    identify_modified_participants_ltpFR2()
