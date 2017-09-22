a2bf13fb69a00c6759c3626486efaea6355061b8
jpazdera <jpazdera@haverford.edu>
2017-09-22 15:12:24 -0400

    Added CRL to verification list

--- subject_reporting/verification/verify_stats.py
+++ subject_reporting/verification/verify_stats.py
@@ -21,7 +21,7 @@ def verify_stats():
         except IOError:
             continue
 
-        fields_to_check = [f for f in stats.keys() if f not in ('p_rec', 'crl')]
+        fields_to_check = [f for f in stats.keys() if f not in ('p_rec')]
         try:
             for field in fields_to_check:
                 new = np.array(stats[field])
