f9f6264866ed6ed8fb520f5ba23227b0bd109db3
jpazdera <jpazdera@haverford.edu>
2018-07-13 15:23:52 -0400

    Bug fix for handling input lists of subjects

--- subject_reporting/reporting_pipeline.py
+++ subject_reporting/reporting_pipeline.py
@@ -86,11 +86,11 @@ def run_pipeline(experiment=None, subjects=None, upload=True):
         # Run on recently modified subjects unless user specified the subjects to use
         if subjects is None:
             with open('/data/eeg/scalp/ltp/%s/recently_modified.json' % exp, 'r') as f:
-                subjs = json.load(f).keys()
+                subjects = json.load(f).keys()
         elif subjects == 'all':
-            subjs = [os.path.basename(s) for s in glob('/data/eeg/scalp/ltp/%s/%s[0-9][0-9][0-9]' % (exp, subj_prefix))]
+            subjects = [os.path.basename(s) for s in glob('/data/eeg/scalp/ltp/%s/%s[0-9][0-9][0-9]' % (exp, subj_prefix))]
 
-        for s in subjs:
+        for s in subjects:
             beh_data = behavioral_func(s)  # Create behavioral data matrices
             # Skip participant if they haven't actually completed any sessions
             if beh_data == {} or statistics_func is None:
