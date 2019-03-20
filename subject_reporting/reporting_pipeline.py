3653ae1a00911f3f8f75d6b224887368169977c9
jpazdera <jpazdera@haverford.edu>
2019-03-20 10:15:22 -0400

    Fixed bug with subject IDs when running reports for multiple experiments

--- subject_reporting/reporting_pipeline.py
+++ subject_reporting/reporting_pipeline.py
@@ -109,12 +109,14 @@ def run_pipeline(experiment=None, subjects=None):
         # Run on recently modified subjects unless user specified a list of subjects to use
         if subjects is None:
             with open('/data/eeg/scalp/ltp/%s/recently_modified.json' % exp, 'r') as f:
-                subjects = json.load(f).keys()
+                subj_list = json.load(f).keys()
         elif subjects == 'all':
-            subjects = [os.path.basename(s) for s in glob('/data/eeg/scalp/ltp/%s/%s[0-9][0-9][0-9]' % (exp, subj_prefix))]
+            subj_list = [os.path.basename(s) for s in glob('/data/eeg/scalp/ltp/%s/%s[0-9][0-9][0-9]' % (exp, subj_prefix))]
+        else:
+            subj_list = subjects
 
         # Run reporting pipeline on each participant
-        for s in subjects:
+        for s in subj_list:
             # Create behavioral data matrices
             beh_data = behavioral_func(s)
 
