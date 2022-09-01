6a8629709f4491d700862f8ca5e44ef032984c4f
Joseph Rudoler <maint@rhino2.rhino.psych.upenn.edu>
2022-09-01 11:06:39 -0400

    Added experiments

--- bonus_reporting/bonus_pipeline.py
+++ bonus_reporting/bonus_pipeline.py
@@ -6,12 +6,14 @@ from bonus_reporting.calculation.bonus_SFR import calculate_bonus_SFR
 from bonus_reporting.calculation.bonus_FR1_scalp import calculate_bonus_FR1_scalp
 from bonus_reporting.calculation.bonus_VFFR import calculate_bonus_VFFR
 from bonus_reporting.calculation.bonus_repfr import calculate_bonus_repfr
+from bonus_reporting.calculation.bonus_courier import calculate_bonus_courier
 
 from bonus_reporting.reports.bonus_report_ltpFR2 import bonus_report_ltpFR2
 from bonus_reporting.reports.bonus_report_SFR import bonus_report_SFR
 from bonus_reporting.reports.bonus_report_FR1_scalp import bonus_report_FR1_scalp
 from bonus_reporting.reports.bonus_report_VFFR import bonus_report_VFFR
 from bonus_reporting.reports.bonus_report_repfr import bonus_report_repfr
+from bonus_reporting.reports.bonus_report_courier import bonus_report_courier
 
 
 def upload_bonus_report(report_path, exp):
@@ -44,7 +46,10 @@ def run_bonus(experiment=None, subjects=None):
         SFR=(calculate_bonus_SFR, bonus_report_SFR, True),
         FR1_scalp=(calculate_bonus_FR1_scalp, bonus_report_FR1_scalp, True),
         VFFR=(calculate_bonus_VFFR, bonus_report_VFFR, True),
-        ltpRepFR=(calculate_bonus_repfr, bonus_report_repfr, True)
+        ltpRepFR=(calculate_bonus_repfr, bonus_report_repfr, True),
+        NiclsCourierReadOnly=(calculate_bonus_courier, bonus_report_courier, True),
+        NiclsCourierClosedLoop=(calculate_bonus_courier,
+            bonus_report_courier, True)
     )
 
     # Determine experiment list
@@ -70,10 +75,17 @@ def run_bonus(experiment=None, subjects=None):
                 subjects = json.load(f).keys()
 
         for s in subjects:
-            scores, bonuses = calculation_func(s)
+            try:
+                scores, bonuses = calculation_func(s, exp=exp)
+            except:
+                scores, bonuses = calculation_func(s)
             if report_func is None:
                 continue
-            _, pdf_path = report_func(s, scores, bonuses)
+            try:
+                _, pdf_path = report_func(s, scores, bonuses, exp=exp)
+            except Exception as e:
+                print(e)
+                _, pdf_path = report_func(s, scores, bonuses)
             if upload:
                 upload_bonus_report(pdf_path, exp)
 
